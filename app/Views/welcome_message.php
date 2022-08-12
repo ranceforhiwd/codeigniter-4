13dee81ba4e5208b1be5451ff345f31d8cf00e93
Rance Aaron <ranceaaron941@gmail.com>
2022-08-12 07:04:33 -0400

    Another test commit

--- app/Views/welcome_message.php
+++ app/Views/welcome_message.php
@@ -220,7 +220,7 @@
 
     <div class="heroe">
 
-        <h1>Welcome to CodeIgniter Rance Test<?= CodeIgniter\CodeIgniter::CI_VERSION ?></h1>
+        <h1>Rance Test <?= CodeIgniter\CodeIgniter::CI_VERSION ?></h1>
 
         <h2>The small framework with powerful features</h2>
 
